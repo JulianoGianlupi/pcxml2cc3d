ad4263333044530106a5648308304b33587b5426
Juliano Ferrari Gianlupi <35964356+JulianoGianlupi@users.noreply.github.com>
2022-09-14 17:38:31 +0200

    finished secretion step gen

--- steppable_gen/generate_secretion_step.py
+++ steppable_gen/generate_secretion_step.py
@@ -5,6 +5,9 @@ except:
     # like this? 1st option does not work when running this file by itself. Second doesn't work when importing the
     # file.............................................................................................................
 
+
+import warnings
+
 # general idea: define secretor objects in start as self variables. Use those in the step function
 # in cell loops. Could I list all secretors and loop them? probably
 
@@ -26,6 +29,7 @@ def make_secretors(field_names):
     sec_list += "}\n"
     return sec_list
 
+
 def make_secretion_loop(ctype, comment):
     loop = generate_cell_type_loop(ctype, 3)
     check_field = "\t\t\t\tif field_name in cell.dict.keys():\n\t\t\t\t\tdata=cell.dict[field_name]\n"
@@ -37,7 +41,7 @@ def make_secretion_loop(ctype, comment):
                     "at the surface. You should explore the options\n"
     secrete = "\t\t\t\t\tsecretor.secreteInsideCell(cell, net_secretion)\n"
 
-    return loop+check_field+seen+comment+secrete_rate+where_secrete+secrete
+    return loop + check_field + seen + comment + secrete_rate + where_secrete + secrete
 
 
 def make_secretion_loops(cell_types, sec_dict, secretors, field_names):
@@ -46,19 +50,29 @@ def make_secretion_loops(cell_types, sec_dict, secretors, field_names):
     loops = ""
     for ctype in cell_types:
         if ctype in sec_dict.keys():
-            comment = sec_dict[ctype][field_names[0]]['secretion_comment']+'\n'
+            comment = sec_dict[ctype][field_names[0]]['secretion_comment'] + '\n'
             loops += make_secretion_loop(ctype, comment)
-    return secretor_loop+loops
+    return secretor_loop + loops
 
 
-def generate_secretion_step(cell_types, sec_dict, first=False):
+def generate_secretion_step(cell_types, sec_dict, secretion_dt=None, first=False):
+
+    if not sec_dict:
+        message = "WARNING: no secretion data found\n"
+        warnings.warn(message)
+        return ''
+
+    if secretion_dt is None:
+        secretion_dt = 1
 
     if not sec_dict:
         return ''
 
     if first:
+        already_imports=False
         imports = steppable_imports()
     else:
+        already_imports=True
         imports = ""
 
     field_names = get_field_names(sec_dict)
@@ -66,6 +80,12 @@ def generate_secretion_step(cell_types, sec_dict, first=False):
     secretors = make_secretors(field_names)
 
     loops = make_secretion_loops(cell_types, sec_dict, secretors, field_names)
+
+    sec_step = generate_steppable("Secretion", secretion_dt, False, additional_start=secretors, additional_step=loops,
+                                  already_imports=already_imports)
+
+    return sec_step
+
 
 
 if __name__ == "__main__":
@@ -82,25 +102,27 @@ if __name__ == "__main__":
                                      'secretion_comment': '#WARNING: PhysiCell has a concept of "target secretion" that CompuCell3D does not. \n#The translating program attempts to implement it, but it may not be a 1 to 1 conversion.',
                                      'net_export_MCS': 0.0, 'net_secretion_comment': '', 'uptake_rate_MCS': 0.0,
                                      'uptake_comment': '#WARNING: To avoid negative concentrations, in CompuCell3D uptake is "bounded." \n# If the amount that would be uptaken is larger than the value at that pixel,\n# the uptake will be a set ratio of the amount available.\n# The conversion program uses 1 as the ratio,\n# you may want to revisit this.'}},
-                    'immune_cell': {
-                        'oxygen': {'secretion_rate': 0.0, 'secretion_unit': '1/min', 'secretion_target': 38.0,
-                                   'uptake_rate': 10.0, 'uptake_unit': '1/min', 'net_export': 0.0,
-                                   'net_export_unit': 'total substrate/min', 'secretion_rate_MCS': 0.0,
-                                   'secretion_comment': '#WARNING: PhysiCell has a concept of "target secretion" that CompuCell3D does not. \n#The translating program attempts to implement it, but it may not be a 1 to 1 conversion.',
-                                   'net_export_MCS': 0.0, 'net_secretion_comment': '', 'uptake_rate_MCS': 1.0,
-                                   'uptake_comment': '#WARNING: To avoid negative concentrations, in CompuCell3D uptake is "bounded." \n# If the amount that would be uptaken is larger than the value at that pixel,\n# the uptake will be a set ratio of the amount available.\n# The conversion program uses 1 as the ratio,\n# you may want to revisit this.'},
-                        'immunostimulatory_factor': {'secretion_rate': 0.0, 'secretion_unit': '1/min',
-                                                     'secretion_target': 1.0, 'uptake_rate': 0.0,
-                                                     'uptake_unit': '1/min', 'net_export': 0.0,
-                                                     'net_export_unit': 'total substrate/min',
-                                                     'secretion_rate_MCS': 0.0,
-                                                     'secretion_comment': '#WARNING: PhysiCell has a concept of "target secretion" that CompuCell3D does not. \n#The translating program attempts to implement it, but it may not be a 1 to 1 conversion.',
-                                                     'net_export_MCS': 0.0, 'net_secretion_comment': '',
-                                                     'uptake_rate_MCS': 0.0,
-                                                     'uptake_comment': '#WARNING: To avoid negative concentrations, in CompuCell3D uptake is "bounded." \n# If the amount that would be uptaken is larger than the value at that pixel,\n# the uptake will be a set ratio of the amount available.\n# The conversion program uses 1 as the ratio,\n# you may want to revisit this.'}}}
+        'immune_cell': {
+            'oxygen': {'secretion_rate': 0.0, 'secretion_unit': '1/min', 'secretion_target': 38.0,
+                       'uptake_rate': 10.0, 'uptake_unit': '1/min', 'net_export': 0.0,
+                       'net_export_unit': 'total substrate/min', 'secretion_rate_MCS': 0.0,
+                       'secretion_comment': '#WARNING: PhysiCell has a concept of "target secretion" that CompuCell3D does not. \n#The translating program attempts to implement it, but it may not be a 1 to 1 conversion.',
+                       'net_export_MCS': 0.0, 'net_secretion_comment': '', 'uptake_rate_MCS': 1.0,
+                       'uptake_comment': '#WARNING: To avoid negative concentrations, in CompuCell3D uptake is "bounded." \n# If the amount that would be uptaken is larger than the value at that pixel,\n# the uptake will be a set ratio of the amount available.\n# The conversion program uses 1 as the ratio,\n# you may want to revisit this.'},
+            'immunostimulatory_factor': {'secretion_rate': 0.0, 'secretion_unit': '1/min',
+                                         'secretion_target': 1.0, 'uptake_rate': 0.0,
+                                         'uptake_unit': '1/min', 'net_export': 0.0,
+                                         'net_export_unit': 'total substrate/min',
+                                         'secretion_rate_MCS': 0.0,
+                                         'secretion_comment': '#WARNING: PhysiCell has a concept of "target secretion" that CompuCell3D does not. \n#The translating program attempts to implement it, but it may not be a 1 to 1 conversion.',
+                                         'net_export_MCS': 0.0, 'net_secretion_comment': '',
+                                         'uptake_rate_MCS': 0.0,
+                                         'uptake_comment': '#WARNING: To avoid negative concentrations, in CompuCell3D uptake is "bounded." \n# If the amount that would be uptaken is larger than the value at that pixel,\n# the uptake will be a set ratio of the amount available.\n# The conversion program uses 1 as the ratio,\n# you may want to revisit this.'}}}
 
-    fields = get_field_names(sdict)
-    secretors = make_secretors(fields)
+    sec_step = generate_secretion_step(['cancer_cell', 'immune_cell'], sdict, first=True)
 
-    loops = make_secretion_loops(['cancer_cell', 'immune_cell'], sdict, secretors, fields)
+    # fields = get_field_names(sdict)
+    # secretors = make_secretors(fields)
+    #
+    # loops = make_secretion_loops(['cancer_cell', 'immune_cell'], sdict, secretors, fields)
     pass
