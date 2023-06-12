dbcd8aa30ab15d36148e0d663cb3e0a328f33410
JulianoGianlupi <julianoferrarigianlupi@gmail.com>
2023-06-12 14:34:49 -0400

    began cell cycle example, there seems to be a bug with phenocell. It is not halving the target volume for the G0G1 phase

---
+++ example-translations/cell_cycle/cell_cycle_mod/Simulation/extra_definitions.py
@@ -0,0 +1,10 @@
+cell_constraints = {
+    'CELL': {
+        'volume': {
+            'volume (None)': None,
+            'volume (pixels)': 8},
+        'mechanics': None,
+        'custom_data': None,
+        'phenotypes': {
+            'Flow Cytometry Basic': None},
+        'phenotypes_names': ['Flow Cytometry Basic']}}
